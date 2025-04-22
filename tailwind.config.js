c13bb7df154e0396ef4eb17baaf4e14a06a53e3c
mariafrisch <fris0039@alumni.umn.edu>
2025-04-22 15:12:28 -0500

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,7 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {
-      animation: {
-        fadeInUp: "fadeInUp 1s ease-out forwards",
-      },
-      keyframes: {
-        fadeInUp: {
-          "0%": { opacity: 0, transform: "translateY(20px)" },
-          "100%": { opacity: 1, transform: "translateY(0)" },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
